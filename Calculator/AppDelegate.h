4671f6f0103235a6f3eb10d505771265b17cacd8
ALEXANDROS BERTZELETOS <Alexandros@ALEXANDROS-BERTZELETOSs-iMac.local>
2011-11-13 22:20:42 +0200

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Alexandros Bertzeletos on 13/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Alexandros Bertzeletos on 13/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
